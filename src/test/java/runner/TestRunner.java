d8f6dfab12d19676137b1b8a830522b2264b378f
npohrib <noemi.pohrib@kingfisher.com>
2025-11-12 16:59:14 +0100

    project

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,14 +5,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-import io.cucumber.junit.CucumberOptions;
-
-import org.junit.runner.RunWith;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "/Users/npohrib/IdeaProjects/Cucumber/src/test/java/features",
-        glue = {"/Users/npohrib/IdeaProjects/Cucumber/src/test/java/stepDefinition","/Users/npohrib/IdeaProjects/Cucumber/src/test/java/utility"}
+        glue = {"stepDefinition","utility"}
 //        plugin = {"pretty", "html:target/cucumber-html-report", "json:cucumber.json" }
 
 )
